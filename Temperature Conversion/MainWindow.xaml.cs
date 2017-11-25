0c95169c48c61e22db1c554128090c684fb20f69
Marc Starrett <Marc.Starrett1@education.nsw.gov.au>
2017-11-25 21:10:36 +1100

    Completed Version - for testing

--- Temperature Conversion/MainWindow.xaml.cs
+++ Temperature Conversion/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Temperature_Conversion
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -28,41 +30,101 @@ namespace Temperature_Conversion
         //This converts Fahrenheit into Celcius 
         public string FtoC()
         {
+            //Checks if there is an input into input box
             if (FahrenheitBox.Text != "")
             {
-                int temp;
-                if (int.TryParse(FahrenheitBox.Text, out temp))
+                //Converts the input into a double data type
+                //Checks if the input is a number
+                double tempF;
+                if (double.TryParse(FahrenheitBox.Text, out tempF)|| FahrenheitBox.Text == "-")
                 {
-                    double C = ((double.Parse(FahrenheitBox.Text) - 32) / 9) * 5;
+                    //If it is a number, an image is allowed to display
+                    TempIMG.Visibility = Visibility.Visible;
+
+                    //If the input into Fahrenheit box is equal to or less than 59, cold graphifical depiction shows
+                    if (tempF <= 59)
+                    {
+
+                        TempIMG.Source = new BitmapImage(new Uri("Resources/COLD.png", UriKind.Relative));
+                    }
+
+                    //If the input into Fahrenheit box is greater than 59 and less than 86, mild graphifical depiction shows
+                    else if (tempF > 59 && tempF < 86)
+                    {
+                        TempIMG.Source = new BitmapImage(new Uri("Resources/MILD.jpg", UriKind.Relative));
+                    }
+
+                    //If the input into Fahrenheit box is equal to or greater than 86, hot graphifical depiction shows
+                    else if (tempF >= 86)
+                    {
+                        TempIMG.Source = new BitmapImage(new Uri("Resources/HOT.jpg", UriKind.Relative));
+                    }
+                    //Converts Fahrenheit to Celcius
+                    double C = (tempF - 32) / 9 * 5;
                     return C.ToString();
+                   
                 }
                 else
                 {
                     MessageBox.Show("Please eneter a number");
                 }
             }
+            //If there is no input into input box no image displays
+            TempIMG.Visibility = Visibility.Hidden;
             return "";
         }
 
-        //This converts Celcius into Fahrenheight
+        //This converts Celcius into Fahrenheit
         public string CtoF()
         {
+            // Checks if there is an input into input box
             if (CelciusBox.Text != "")
             {
-                int temp;
-                if (int.TryParse(CelciusBox.Text, out temp))
+                //Converts the input text into a double data type
+                //Checks if the input text is a number
+                double temp;
+                if (double.TryParse(CelciusBox.Text, out temp)|| CelciusBox.Text == "-")
                 {
-                    double F = ((double.Parse(CelciusBox.Text) * 9) / 5) + 32;
+                    TempIMG.Visibility = Visibility.Visible;
+
+                    //If the input into Celcius box is equal to or less than 15, cold graphifical depiction shows
+                    if (temp <= 15)
+                    {
+                        //If it is a number, an image is allowed to display
+                        TempIMG.Source = new BitmapImage(new Uri("Resources/COLD.png", UriKind.Relative));
+                    }
+
+                    //If the input into Celcius box is greater than 15 and less than 30, mild graphifical depiction shows
+                    else if (temp > 15 && temp < 30)
+                    {
+                            TempIMG.Source = new BitmapImage(new Uri("Resources/MILD.jpg", UriKind.Relative));         
+                    }
+
+                    //If the input into Celcius box is equal to or greater than 30, hot graphifical depiction shows
+                    else if (temp >= 30)
+                    {
+                            TempIMG.Source = new BitmapImage(new Uri("Resources/HOT.jpg", UriKind.Relative));
+                    }
+
+                    //Converts Celcius to Fahrenheit
+                    double F = (temp * 9) / 5 + 32;
                     return F.ToString();
+
                 }
                 else
                 {
+                    
                     MessageBox.Show("Please enter a number");
                 }
             }
+            //If there is no input into input box no image displays
+            TempIMG.Visibility = Visibility.Hidden;
             return "";
+
         }
 
+
+        //Automatically converts inputs from Fahrenheight to Celcius
        private void FahrenheitBox_TextChanged(object sender, TextChangedEventArgs e)
        {
             if (FahrenheitBox.IsFocused)
@@ -74,6 +136,7 @@ namespace Temperature_Conversion
        }
 
 
+        //Automatically converts inputs from Celcius to Fahrenheight
         private void CelciusBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (CelciusBox.IsFocused)
@@ -83,10 +146,12 @@ namespace Temperature_Conversion
             }
         }
 
+        //Clears both text boxes
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             CelciusBox.Clear();
             FahrenheitBox.Clear();
         }
+
     }
 }
